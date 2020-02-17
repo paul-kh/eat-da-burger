d6d23c5052ff7a917b293712a06bf0d2677fd957
Paul Chheang <phalcheat@gmail.com>
2020-02-17 16:00:02 -0600

    refactored codes

--- public/assets/controllers/burger-controller2.js
+++ public/assets/controllers/burger-controller2.js
@@ -26,8 +26,12 @@ function addBurger() {
 document.getElementById("submit-btn").addEventListener("click", event => {
     addBurger();
     renderContent();
-    devourIt(); // enable click event of the button "Devour it" right after a new burger is added
+    getDevourBtns();
+    // devourIt(); // enable click event of the button "Devour it" right after a new burger is added
 });
+getBurgerData();
+renderContent();
+getDevourBtns();
 
 // *** Function that request burger data from the server/database.
 //     The function return burger data received from the server.
@@ -61,7 +65,7 @@ async function renderContent() {
             return `<div class="row d-flex justify-content-center">
                             <div class="col-8 col-md-7 col-lg-8 burger-js text-left">${burger.id}. ${burger.name}</div>
                             <div class="col-4 col-md-5 col-lg-4">
-                                <button id="${burger.id}" class="btn-sm btn-warning devour-btn-js" type="submit"> Devour it!</button>
+                                <button class="btn-sm btn-warning devour-btn-js" id="${burger.id}" type="submit"> Devour it!</button>
                             </div>
                         </div>`;
         }
@@ -72,25 +76,53 @@ async function renderContent() {
 
     // creates object to store the mapping of 'Devour it' button elements and new burger elements
     const devourBtnEls = document.querySelectorAll(".devour-btn-js");
-    // const burgerEls =  document.querySelectorAll(".burger-js");
-    // const devourObj = { devourBtns: devourBtnEls, burgers: burgerEls };
-    return devourBtnEls;
+    const burgerEls =  document.querySelectorAll(".burger-js");
+    const devourObj = { devourBtns: devourBtnEls, burgers: burgerEls };
+    return devourObj;
 }
+
+// *** Function that maps the 'Devour it' buttons and their respective burger elements (div).
+// The function returns an array containing the following objects: 'Devour it' buttons and new burger elements
+// ===========================================================================================================
+// async function mapDevourBtnsAndBurgers() {
+//     const devourObj = await renderContent();
+//     const devourBtns = devourObj.devourBtns;
+//     const newBurgers = devourObj.burgers;
+//     // Create an array to map the new burgers and their respective 'Devour it' buttons.
+//     // The array structure should be: [{burgerId: '1', burgerName: "xxx", buttonEl: "btnObj" }]
+//     const toDevourArr = []; 
+//     for (let i = 0; i < devourBtns.length; i++) {
+//         toDevourArr.push({ "burgerId": newBurgers[i].innerHTML.split(".")[0], "burgerName": newBurgers[i].innerHTML.split(".")[1], "button": devourBtns[i] });
+//         // *** Note: By splitting the array newBurgers at the dot "." sign then get item before dot for 'burger Id' and item after the dot for burger's name.
+//     }
+//     return toDevourArr;
+// }
 
 // *** Function that handles click event when users click each "Devour it" button
 // ================================================================
-async function devourIt() {
-    const devourBtns = await renderContent();
+// async function devourIt(){
+//     let toDevourArr = await mapDevourBtnsAndBurgers();
+//     for (let i = 0; i < toDevourArr.length; i++) {
+//         toDevourArr[i].button.addEventListener("click", async function () {
+//             axios.post('/api/burgers', {
+//                 id: toDevourArr[i].burgerId,
+//             }).then(function (res) {
+//                 renderContent(); // moves the devoured burger to the right sidebar and updates database
+//                 devourIt(); // refresh the array toDevourArr with the remaining "devour it" button elements and the elements of new-burger div
+//             }).catch(function (err) {
+//                 console.log(err);
+//             });   
+//         });  
+//     }
+// }
+// devourIt();
 
-    for (let i = 0; i < devourBtns.length; i++) {
-        devourBtns[i].addEventListener("click", ({ target }) => {
-            axios.post('/api/burgers', {
-                id: target.id,
-            }).then(function (res) {
-                renderContent(); // moves the devoured burger to the right sidebar and updates database
-            }).catch(function (err) {
-                console.log(err);
-            });
+function getDevourBtns(){
+    const devourBtns = document.querySelectorAll(".devour-btn-js");
+    for (let i = 0; i < devourBtns.length; i++){
+        devourBtns[i].addEventListener("click", function (){
+            console.log("devour btn id: ", event.target.id);
         });
     }
 }
+getDevourBtns();
